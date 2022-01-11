6d59dc438ac696265b421df43cadb309cb22d685
gauravsaboo <gauravsaboo1@gmail.com>
2022-01-11 21:03:18 +0530

    Cleaning repo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <h1>Covid Tracker</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 }
