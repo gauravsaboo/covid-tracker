9e54c74d85eebf2e05e144a97ffbc4dd516d63dc
gauravsaboo <gauravsaboo1@gmail.com>
2022-01-26 00:23:02 +0530

    Map and Line Graph Added

--- src/Table.js
+++ src/Table.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './Table.css';
+import React from "react";
+import "./Table.css";
+import numeral from "numeral";
 
 function Table({ countries }) {
-
   return (
     <div className="table">
-      {countries.map(({ country, cases }) => (
+      {countries.map((country) => (
         <tr>
-          <td>{country}</td>
-          <strong>
-            <td>{cases}</td>
-          </strong>
+          <td>{country.country}</td>
+          <td>
+            <strong>{numeral(country.cases).format("0,0")}</strong>
+          </td>
         </tr>
       ))}
     </div>
